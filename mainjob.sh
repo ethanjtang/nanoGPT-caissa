4282129a946426c865105810e2e8332c0f9defd7
ethanjtang <ethantang2026@gmail.com>
2025-11-06 19:15:00 -0700

    to test on sol

---
+++ mainjob.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+#SBATCH -p general ## Partition
+#SBATCH -q public  ## QOS
+#SBATCH -N 1      ## Number of Sol Nodes
+#SBATCH -c 8     ## Number of Cores
+#SBATCH --mem=32G  ## Memory (GB)
+#SBATCH --time=10  ## Minutes of compute
+#SBATCH -G 1        ## Number of GPUs
+#SBATCH --job-name=ood-example-python
+#SBATCH --output=slurm.%j.out  ## job /dev/stdout record (%j expands -> jobid)
+#SBATCH --error=slurm.%j.err   ## job /dev/stderr record 
+#SBATCH --export=NONE          ## keep environment clean
+#SBATCH --mail-type=ALL        ## notify <asurite>@asu.edu for any job state change
+
+echo "=========================================="
+echo "Mini Caissa Test"
+echo "=========================================="
+
+# Load environment
+module load mamba/latest
+source activate caissa-env
+
+# Install dependencies
+echo "Installing mamba stuffs"
+mamba install torch
+
+echo "Installing Python stuffs"
+pip install torch numpy transformers datasets tiktoken wandb tqdm
+
+# Prepare training data
+echo "Preparing training data"
+python data/annotated-games/prepare.py
+
+# Train LLM
+echo "Training LLM on data"
+python train.py config/train_caissa.py
+
+# Sample from LLM
+python sample.py --out_dir=out-annotated-games
+
+
+
