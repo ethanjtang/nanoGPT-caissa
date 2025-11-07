4b1a3cb95966620be14cdcd477057804e9e351f2
ethanjtang <ethantang2026@gmail.com>
2025-11-06 19:07:30 -0700

    change to use my own data yippee

--- config/train_caissa.py
+++ config/train_caissa.py
@@ -1,19 +1,19 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-shakespeare-char'
+out_dir = 'out-annotated-games'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
-log_interval = 10 # don't print too too often
+log_interval = 1 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
 wandb_log = False # override via command line if you like
-wandb_project = 'shakespeare-char'
-wandb_run_name = 'mini-gpt'
+wandb_project = '5-annotated-game-sample'
+wandb_run_name = 'mini-caissa'
 
-dataset = 'shakespeare_char'
+dataset = 'annotated-games'
 gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
@@ -33,5 +33,5 @@ beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+device = 'cpu'  # run on cpu only
+compile = False # do not torch compile the model
